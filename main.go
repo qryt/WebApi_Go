ce11802ba10a284c7496a8a3392d0dbc627b29a4
李海涛 <192938976@qq.com>
2021-08-19 09:57:34 +0800

    添加日志

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	. "WebApi/Controller"
 	"WebApi/FrameWork"
 	"github.com/gin-gonic/gin"
+	"io"
+	"os"
 )
 
 func main() {
+
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f)
 	c := gin.Default()
 	control := new(TestConterller)
 	c.GET("Insert1", control.Insert1)
